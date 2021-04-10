cdd5c380fc2a43e4b9b51654d09a9fdadadaa154
PCunha13 <pcunha13@gmail.com>
2021-04-10 22:41:39 +0100

    post request

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  //res.status(200).send("Hello from the server side!"); //send é respota
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" });
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+//get all tours
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
+
+//creat a new tour
+app.post("/api/v1/tours", (req, res) => {
+  //nao tenho base de dados, vou buscar o ultimo e adiciono 1
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  //writeFile pq estou dentro de uma callback, nao quero bloquear codigo
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
+
 const port = 3000;
 
-//iniciar servidor
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
