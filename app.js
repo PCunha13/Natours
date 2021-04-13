ea54d233d57b727d639a1c61028c1736fa44200f
PCunha13 <pcunha13@gmail.com>
2021-04-13 19:32:11 +0100

    mongoDB

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require("./routes/tourRoutes"); //import module
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes'); //import module
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 //1) MIDDLEWARES
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/puclic`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware!!");
+  // eslint-disable-next-line no-console
+  console.log('Hello from the middleware!!');
   next();
 });
 
@@ -25,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 //3) ROUTES
-app.use("/api/v1/tours", tourRouter); //mounting router
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter); //mounting router
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
