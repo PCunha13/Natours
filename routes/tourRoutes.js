a3710cfdc3ee976a2efd92f0167ff3510329feb2
PCunha13 <pcunha13@gmail.com>
2021-04-15 19:18:43 +0100

    newDoc

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,12 +3,12 @@ const tourController = require('./../controllers/tourController');
 //const {getAllTours,creatTour,getTour,updateTour,deleteTour} = require("./../controllers/tourController");
 const router = express.Router();
 
-router.param('namePrice', tourController.checkBody);
+//router.param('namePrice', tourController);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.creatTour);
+  .post(tourController.creatTour);
 
 router
   .route('/:id')
